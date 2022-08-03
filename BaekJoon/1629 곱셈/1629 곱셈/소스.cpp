3f3fc96ce63fdd576919a4b1fa0ade114fedae30
이동율 <dy9623@naver.com>
2022-08-03 12:53:24 +0900

    Solve again the problem

--- BaekJoon/1629 곱셈/1629 곱셈/소스.cpp
+++ BaekJoon/1629 곱셈/1629 곱셈/소스.cpp
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
-	long long a, b, c;
-	scanf("%lld%lld%lld",&a, &b, &c);
-	int cnt = 0;
-	long long res = a;
-	while (cnt != b) {
-		res *= a;
-		cnt++;
+using namespace std;
+
+int pow(long long a, long long b,long long c) {
+	if (b == 0) return 1;
+	else {
+		long n = pow(a, b / 2,c);
+		int temp = n * n % c;
+		if (b % 2 == 0) {
+			return temp;
+		}
+		else return (a * temp)%c;
 	}
-	printf("%d", res % c);
+}
+
+int main() {
+	long long a, b, c;
+	cin >> a >> b >> c;
+	cout << pow(a, b, c);
+
 }
